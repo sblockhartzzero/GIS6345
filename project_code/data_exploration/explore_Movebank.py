4349112d90b218e5ad7d9456947bc35455b2485c
Stephen Lockhart <sblockhart.zzero@gmail.com>
2025-10-10 12:48:05 -0400

    Convert track timestamp to datetime

--- project_code/data_exploration/explore_Movebank.py
+++ project_code/data_exploration/explore_Movebank.py
@@ -10,6 +10,8 @@ Explore shapefile downloaded from movebank.org
 import fiona
 from shapely.geometry import shape
 import geopandas as gpd
+import pandas as pd
+from datetime import datetime
 
 
 # Specify fullpath to input shapefile
@@ -34,6 +36,32 @@ input_gdf = gpd.read_file(input_shp)
 
 # Preview
 print(input_gdf.head())
+
+# Since shapefile does not contain timestamps, use csv instead, starting
+# with a pandas dataframe
+
+# Specify fullpath to csv
+# i.e. csv downloaded from movebank.org
+input_csv = 'C:/Users/s44ba/git/GIS6345/project_data/track_data/Short_finned_pilot_whales_CRC_NW_Atlantic.csv'
+df = pd.read_csv(input_csv)
+print(df.info())
+
+# Fewer columns
+df_subset = df[['event-id','timestamp','location-long','location-lat','individual-local-identifier']]
+
+# Convert to datetime
+# Add new column, derived timestamp_datetime, initialized to 1/1/1970
+df_subset["timestamp_datetime"] = datetime(1970,1,1)
+# Convert
+format_string = "%Y-%m-%d %H:%M:%S.000"
+for k in range(len(df_subset)):
+    datetime_string = df_subset.loc[k,"timestamp"]
+    df_subset.loc[k,"timestamp_datetime"] = datetime.strptime(datetime_string,format_string)
+#endfor
+print(df_subset.head())    
+
+
+
     
     
 
